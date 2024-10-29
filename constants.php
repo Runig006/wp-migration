ef42ce63e956c68e8d81f4fee4ef0867e8d41e13
Alex Osendi <alexcomputer14@gmail.com>
2024-10-29 13:55:36 +0100

    Even bigger

--- constants.php
+++ constants.php
@@ -281,7 +281,7 @@ define( 'AI1WM_SUPPORT_EMAIL', 'support@servmask.com' );
 // =================
 // = Max File Size =
 // =================
-define( 'AI1WM_MAX_FILE_SIZE', 536870912 * 50 );
+define( 'AI1WM_MAX_FILE_SIZE', 536870912 * 500 );
 
 // ==================
 // = Max Chunk Size =
